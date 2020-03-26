2ea5948c3c34212ae49a077ec33897ac7ec2a551
hope-john <36391218+hope-john@users.noreply.github.com>
2020-03-26 21:31:52 +0700

    Select_Months

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Header from './Component_Header' 
 import Component_Stock from'./Component_Stock'
+import Component_Select_Month from'./Component_Select_Month'
 import './App.css'
 
 
@@ -11,6 +12,8 @@ export default class App extends Component {
       <Header/>
       <br></br>
       <Component_Stock/>
+      <br></br>
+      <Component_Select_Month/>
       </div>
     )
   }
