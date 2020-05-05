6e9ac3c2573e3047a76c3bcc57f01276cb33ff1d
hope-john <36391218+hope-john@users.noreply.github.com>
2020-05-05 20:20:39 +0700

    add more Graph

--- src/components/Result.js
+++ src/components/Result.js
@@ -6,6 +6,28 @@ export default class Result extends Component {
             <div>
                 <h3 className="front"> Prediction Result in a year</h3>
                 <img src = "Graph_year.png" alt="Grap" className="Pic1"></img>
+
+                <img src = "Graph_year1.png" alt="Grap1" className="Pic1"></img>
+                <img src = "Graph_year2.png" alt="Grap1" className="Pic1"></img>
+                <img src = "Graph_year3.png" alt="Grap1" className="Pic1"></img>
+                <img src = "Graph_year4.png" alt="Grap1" className="Pic1"></img>
+                <img src = "Graph_year5.png" alt="Grap1" className="Pic1"></img>
+                <img src = "Graph6.png" alt="Grap1" className="Pic1"></img>
+                <img src = "Graph7.png" alt="Grap1" className="Pic1"></img>
+                <img src = "Graph8.png" alt="Grap1" className="Pic1"></img>
+                <img src = "Graph9.png" alt="Grap1" className="Pic1"></img>
+                <img src = "Graph10.png" alt="Grap1" className="Pic1"></img>
+                <img src = "Graph11.png" alt="Grap1" className="Pic1"></img>
+                <img src = "Graph12.png" alt="Grap1" className="Pic1"></img>
+                <img src = "Graph13.png" alt="Grap1" className="Pic1"></img>
+                <img src = "Graph14.png" alt="Grap1" className="Pic1"></img>
+                <img src = "Graph15.png" alt="Grap1" className="Pic1"></img>
+                <img src = "Graph16.png" alt="Grap1" className="Pic1"></img>
+                <img src = "Graph17.png" alt="Grap1" className="Pic1"></img>
+                <img src = "Graph18.png" alt="Grap1" className="Pic1"></img>
+                <img src = "Graph19.png" alt="Grap1" className="Pic1"></img>
+                <img src = "Graph20.png" alt="Grap1" className="Pic1"></img>
+                
             </div>
         )
     }
