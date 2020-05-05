9eeeb88e5f0e7a191a0e0e6b6652fa6e5c98d049
hope-john <36391218+hope-john@users.noreply.github.com>
2020-05-05 20:47:31 +0700

    Add text name of Stock

--- src/components/Result.js
+++ src/components/Result.js
@@ -5,27 +5,48 @@ export default class Result extends Component {
         return (
             <div>
                 <h3 className="front"> Prediction Result in a year</h3>
+                <h5 className="Text"> AIS</h5>
+            
                 <img src = "Graph_year.png" alt="Grap" className="Pic1"></img>
-
+                <h5 className="Text"> Airports of Thailand</h5>
                 <img src = "Graph_year1.png" alt="Grap1" className="Pic1"></img>
+                <h5 className="Text"> Asset World Corp</h5>
                 <img src = "Graph_year2.png" alt="Grap1" className="Pic1"></img>
+                <h5 className="Text"> Banpu</h5>
                 <img src = "Graph_year3.png" alt="Grap1" className="Pic1"></img>
+                <h5 className="Text"> Bangkok Bank Public Company</h5>
                 <img src = "Graph_year4.png" alt="Grap1" className="Pic1"></img>
+                <h5 className="Text"> Bangkok Dusit Medical</h5>
                 <img src = "Graph_year5.png" alt="Grap1" className="Pic1"></img>
+                <h5 className="Text"> B.Grimm Power</h5>
                 <img src = "Graph6.png" alt="Grap1" className="Pic1"></img>
+                <h5 className="Text"> Bumrungrad Hospital</h5>
                 <img src = "Graph7.png" alt="Grap1" className="Pic1"></img>
+                <h5 className="Text"> Berli Jucker</h5>
                 <img src = "Graph8.png" alt="Grap1" className="Pic1"></img>
+                <h5 className="Text"> BTS Group Holdings</h5>
                 <img src = "Graph9.png" alt="Grap1" className="Pic1"></img>
+                <h5 className="Text"> Bangkok Express</h5>
                 <img src = "Graph10.png" alt="Grap1" className="Pic1"></img>
+                <h5 className="Text"> Carabao Group</h5>
                 <img src = "Graph11.png" alt="Grap1" className="Pic1"></img>
+                <h5 className="Text"> CP-ALL</h5>
                 <img src = "Graph12.png" alt="Grap1" className="Pic1"></img>
+                <h5 className="Text"> Charoen Pokhand Foods</h5>
                 <img src = "Graph13.png" alt="Grap1" className="Pic1"></img>
+                <h5 className="Text"> Central Group</h5>
                 <img src = "Graph14.png" alt="Grap1" className="Pic1"></img>
+                <h5 className="Text"> Central Retail Coraporation</h5>
                 <img src = "Graph15.png" alt="Grap1" className="Pic1"></img>
+                <h5 className="Text"> Delta Electronics Thailand</h5>
                 <img src = "Graph16.png" alt="Grap1" className="Pic1"></img>
+                <h5 className="Text"> DTAC</h5>
                 <img src = "Graph17.png" alt="Grap1" className="Pic1"></img>
+                <h5 className="Text"> Energy Absolute</h5>
                 <img src = "Graph18.png" alt="Grap1" className="Pic1"></img>
+                <h5 className="Text"> Electricity Generating</h5>
                 <img src = "Graph19.png" alt="Grap1" className="Pic1"></img>
+                <h5 className="Text"> Siam Global House</h5>
                 <img src = "Graph20.png" alt="Grap1" className="Pic1"></img>
                 
             </div>
